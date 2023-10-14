8bef2cba683e53741e49b48c6b1f1958e917314b
Farid Nabil Firdaus <92703687+Faridnabil@users.noreply.github.com>
2023-10-14 16:01:38 +0700

    Update Template (NOT FIX)

---
+++ database/seeders/OfficeProfileSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\OfficeProfile;
+use Illuminate\Database\Seeder;
+
+class OfficeProfileSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        OfficeProfile::create([
+            'name' => 'PT. Nuryeni ',
+            'email' => 'pt.nuryeni2018@gmail.com',
+            'telepon' => '(0264) 88305518',
+            'alamat' => 'Jl. Pramuka, Purwakarta, Indonesia',
+            'whatsapp' => null,
+            'instagram' => null,
+            'facebook' => null,
+            'linkedin' => null,
+            'youtube' => null,
+            'maps' => 'https://goo.gl/maps/TSFRY1FHQkHWhtoK9',
+            'logo' => 'file/profilePT/nyn.png',
+            'about' => 'PT. Nuryeni adalah sebuah perusahaan yang bergerak dibidang Waste (LB3) management mulai dari penyewaan alat berat dll',
+            'pengalaman' => '0',
+            'anggota' => '0',
+            'penilaian' => '0',
+            'proyek' => '0',
+        ]);
+    }
+}
---
+++ resources/views/pages/landingPage/service.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.layout_landing')
+
+@section('title')
+Nuryeni | Solution To All Your Problems
+@endsection
+
+@section('content')
+<!-- Page Header Start -->
+<div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <h1 class="display-3 text-white animated slideInRight">Service</h1>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb animated slideInRight mb-0">
+                <li class="breadcrumb-item"><a href="/">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Service</li>
+            </ol>
+        </nav>
+    </div>
+</div>
+<!-- Page Header End -->
+
+<!-- Service Start -->
+@if(count($services) > 0)
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center mx-auto pb-4 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+            <p class="fw-medium text-uppercase text-primary mb-2">Our Services</p>
+            <h1 class="display-5 mb-4">Kami Memberikan Pelayanan Terbaik</h1>
+        </div>
+        <div class="row gy-5 gx-4">
+
+            @foreach ($services as $service)
+            <div class="col-md-6 col-lg-4 wow fadeInUp mb-4" data-wow-delay="0.1s">
+                <div class="service-item">
+                    <img class="img-fluid service-img-cover" src="{{ $service->file ? asset($service->file) : asset('assetsLanding/img/placeholder.jpg') }}" alt="service">
+                    <div class="service-img">
+                        <img class="img-fluid" src="{{ $service->file ? asset($service->file) : asset('assetsLanding/img/placeholder.jpg') }}" alt="service">
+                    </div>
+                    <div class="service-detail">
+                        <div class="service-title">
+                            <hr class="w-25">
+                            <h3 class="mb-0">{{ $service->title }}</h3>
+                            <hr class="w-25">
+                        </div>
+                        <div class="service-text">
+                            <p class="text-white mb-0">{{$service->description}}</p>
+                        </div>
+                    </div>
+                    {{-- <a class="btn btn-light" href="">Read More</a> --}}
+                </div>
+            </div>
+            @endforeach
+
+            {!! $services->withQueryString()->links('pagination::bootstrap-5') !!}
+
+        </div>
+    </div>
+</div>
+@endif
+<!-- Service End -->
+
+
+@endsection
